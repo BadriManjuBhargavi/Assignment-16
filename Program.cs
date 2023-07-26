f17b83f1cd01eb46d181daa0c66f1ab30dab4f28
manju12345 <manju@gmail.com>
2023-07-26 15:42:02 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Creating a bank account and making some transactions
+        BankAccount account = new BankAccount("John Doe");
+
+        Console.WriteLine($"Account Number: {account.AccountNumber}");
+        Console.WriteLine($"Account Holder Name: {account.AccountHolderName}");
+        Console.WriteLine($"Initial Balance: {account.balance}");
+
+        account.Deposit(1000);
+        Console.WriteLine($"Balance after deposit: {account.balance}");
+
+        account.Withdraw(500);
+        Console.WriteLine($"Balance after withdrawal: {account.balance}");
+
+        try
+        {
+            account.Withdraw(700); // This will throw an exception due to insufficient funds
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
